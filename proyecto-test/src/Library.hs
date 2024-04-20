c73f44afefba70353dcc7f2259a89e809df51bd7
Axel Herbas <axelherbas.446@gmail.com>
2024-04-20 01:28:42 -0300

    Agrego soluciones al issue 2

--- proyecto-test/src/Library.hs
+++ proyecto-test/src/Library.hs
@@ -44,12 +44,44 @@ maipu = Ciudad{
   costoDeVida = 115
 }
 
---Funciones Punto 1
-
+-- Issue 1
 -- Elimino subtract y lo reemplazo por una suma, y ademas anioFundacion ciudad lo convierto en negativo
+-- Es correcto asi?
 valorCiudad :: Ciudad -> Number
 valorCiudad ciudad | anioFundacion ciudad < 1800          =  (+1800) (- anioFundacion ciudad)
                    | null (atraccionesPrincipales ciudad) = costoDeVida ciudad *2
                    | otherwise                           = costoDeVida ciudad *3
 
--- 
+
+-- Issue 2.1
+-- Cambio de nombres a unos mas expresivos. Son adecuados?
+esVocal :: Char -> Bool
+esVocal letra = letra `elem` "aeiouAEIOU"
+
+esUnaAtraccionCopada :: String -> Bool
+esUnaAtraccionCopada (primeraLetra:_) = esVocal primeraLetra
+
+tieneAtraccionesCopadas :: LaCiudadEs
+tieneAtraccionesCopadas ciudad = any esUnaAtraccionCopada (atraccionesPrincipales ciudad)
+
+
+-- Issue 2.2
+aumentarPorcentaje :: Number -> Number -> Number
+aumentarPorcentaje aumento costo = aumento * costo / 100 + costo
+
+-- bajarCostodeVida ahora recibe una Ciudad y retorna un numero. Es correcto?
+-- O es preferible no modificar la funcion y solo cambiar el nombre a "reducir porcentaje"?
+bajarCostodeVida :: Ciudad -> Number 
+bajarCostodeVida ciudad = aumentarPorcentaje (-10) (costoDeVida ciudad)
+
+-- Cambio de nombres en la recursividad. Es más expresivo de esta forma?
+quitarUltimaAtraccion :: [a] -> [a]
+quitarUltimaAtraccion [] = []  -- caso base
+quitarUltimaAtraccion [primeraAtraccion] = [] -- caso unitario
+quitarUltimaAtraccion (primeraAtraccion : atracciones) = primeraAtraccion : quitarUltimaAtraccion atracciones  -- recursividad
+
+invocarCrisis :: Ciudad -> Ciudad
+invocarCrisis ciudad = ciudad {
+                        atraccionesPrincipales = quitarUltimaAtraccion (atraccionesPrincipales ciudad),
+                        costoDeVida            = bajarCostodeVida ciudad
+                    }
