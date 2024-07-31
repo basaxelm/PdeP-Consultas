14447402960ba4e429eb2d1264577f9f59466641
Axel Herbas <axelherbas.446@gmail.com>
2024-07-30 21:10:44 -0300

    Agrego resolucion del tp de funcional

--- proyecto-test/src/Library.hs
+++ proyecto-test/src/Library.hs
@@ -2,86 +2,232 @@ module Library where
 import PdePreludat
 import ShowFunction
 
-type LaCiudadEs = Ciudad -> Bool
+-- dominio
 
-data Ciudad = Ciudad{
-        nombre :: String,
-        anioFundacion :: Number,
-        atraccionesPrincipales :: [String],
-        costoDeVida :: Number
+data Ciudad = Ciudad {
+    nombre :: String,
+    añoFundacion :: Number,
+    atraccionesPrincipales :: [String],
+    costoDeVida :: Number
 } deriving Show
 
-baradero = Ciudad{
+baradero = Ciudad {
     nombre = "Baradero",
-    anioFundacion = 1615,
-    atraccionesPrincipales = ["Parque del Este", "Museo Alejandro Barbich"],
+    añoFundacion = 1615,
+    atraccionesPrincipales = ["Parque del Este", "Museo Alejando Barbich"],
     costoDeVida = 150
 }
-nullish = Ciudad{
+
+nullish = Ciudad {
     nombre = "Nullish",
-    anioFundacion = 1800,
+    añoFundacion = 1800,
     atraccionesPrincipales = [],
-    costoDeVida = 140 
+    costoDeVida = 140
 }
-caletaOlivia = Ciudad{
+
+caletaOlivia = Ciudad {
     nombre = "Caleta Olivia",
-    anioFundacion = 1901,
-    atraccionesPrincipales = ["El Gorosito","Faro Costanera"],
+    añoFundacion = 1901,
+    atraccionesPrincipales = ["El Gorosito", "Faro Costanera"],
     costoDeVida = 120
+}
+
+maipu = Ciudad {
+    nombre = "Maipu",
+    añoFundacion = 1878,
+    atraccionesPrincipales = ["Fortin Kakel"],
+    costoDeVida = 115
+}
+
+azul = Ciudad {
+    nombre = "Azul",
+    añoFundacion = 1832,
+    atraccionesPrincipales = ["Teatro Español", "Parque Municipal Sarmiento", "Costanera Cacique Catriel"],
+    costoDeVida = 190
+}
+
+
+-- Funciones
+{- Punto 1
+    Definir el valor de una ciudad, un número que se obtiene de la siguiente manera:
+    a) Si fue fundada antes de 1800, su valor es 5 veces la diferencia entre 1800 y el año de fundación
+    b) Si no tiene atracciones, su valor es el doble del costo de vida
+    c) De lo contrario, será 3 veces el costo de vida de la ciudad
+ -}
+
+valorCiudad ciudad 
+            | añoFundacion ciudad < 1800 = (*5) (1800 - añoFundacion ciudad)
+            | null (atraccionesPrincipales ciudad) = costoDeVida ciudad * 2
+            | otherwise = costoDeVida ciudad * 3
+
+
+{- Punto 2.1
+    Queremos saber si una ciudad tiene alguna atracción copada, esto es que la atracción comience con una vocal. Por ejemplo:"Acrópolis" es una atracción copada y "Golden Gate" no es copada
+ -}
+
+isVowel :: Char -> Bool
+isVowel character = character `elem` "aeiouAEIOU"
+
+verificarPrimeraLetra = isVowel . head 
+
+tieneAtraccionesCopadas ciudad = any verificarPrimeraLetra $ atraccionesPrincipales ciudad
+
+
+{- Punto 2.2
+    Queremos saber si una ciudad es sobria, esto se da si todas las atracciones tienen más de x letras. El valor x tiene que poder configurarse.
+ -}
+
+verificarCantidadDeLetras cantidad lista = length lista > cantidad
+
+ciudadSobria cantidad ciudad = all (verificarCantidadDeLetras cantidad) (atraccionesPrincipales ciudad)
+
+
+{- Punto 2.3
+    Queremos saber si una ciudad tiene un nombre raro, esto implica que tiene menos de 5 letras en su nombre. Recuerde que no puede definir funciones auxiliares.
+ -}
+
+tieneNombreRaro = (<5) . length . nombre 
+
+
+{- Punto 3
+    Queremos poder agregar una nueva atracción a la ciudad. Esto implica un esfuerzo de toda la comunidad en tiempo y dinero, lo que se traduce en un incremento del costo de vida de un 20%.
+ -}
+
+agregarNuevaAtraccion atraccion ciudad = ciudad {
+                                        atraccionesPrincipales = atraccion : atraccionesPrincipales ciudad,
+                                        costoDeVida = costoDeVida ciudad + costoDeVida ciudad * 0.2
+                                    }
+
+
+{- Punto 3.1
+    Al atravesar una crisis, la ciudad baja un 10% su costo de vida y se debe cerrar la última atracción
+ -}
+
+crisis ciudad = ciudad {
+    atraccionesPrincipales = init (atraccionesPrincipales ciudad),
+    costoDeVida = costoDeVida ciudad - costoDeVida ciudad * 0.1
+}
+
+
+{- Punto 3.2
+    Al remodelar una ciudad, incrementa su costo de vida un x% (valor que se quiere configurar) y le agrega el prefijo "New " al nombre.
+ -}
+
+remodelar valor ciudad = ciudad {
+    nombre = "New" ++ " " ++ nombre ciudad,
+    costoDeVida = costoDeVida ciudad + costoDeVida ciudad * (valor / 100)
 } 
 
-azul = Ciudad{
-    nombre = "Azul",
-    anioFundacion = 1832,
-    atraccionesPrincipales = ["Teatro Español", "Parque Municipal Sarmiento", "Costanera Cacique Catriel"],
-    costoDeVida = 190 
+
+{- Punto 3.3
+    Si la ciudad es sobria con atracciones de más de n letras (valor que se quiere configurar), aumenta el costo de vida un 10%, si no baja 3 puntos.
+ -}
+
+reevaluar valor ciudad | ciudadSobria valor ciudad = ciudad { costoDeVida = costoDeVida ciudad + costoDeVida ciudad * 0.1 }
+                       | otherwise = ciudad { costoDeVida = costoDeVida ciudad - 3 }
+
+
+{- Punto 5.1
+    Queremos modelar un año, donde definamos
+    . el número que le corresponde
+    . una serie de eventos que se produjeron
+    También queremos reflejar el paso de un año para una ciudad, es decir, que los eventos afecten el estado final en el que queda una ciudad.
+ -}
+
+type Evento = Ciudad -> Ciudad
+data Anio = Anio {
+    anio :: Number,
+    eventos :: [Evento]
+} deriving Show
+
+dosmilveintitres = Anio {
+    anio = 2023,
+    eventos = [crisis, agregarNuevaAtraccion "Parque", remodelar 10, remodelar 20]
 }
 
-maipu = Ciudad{
-  nombre = "Maipu",
-  anioFundacion = 1878,
-  atraccionesPrincipales =["Fortin Kakel"],
-  costoDeVida = 115
+dosmilveintidos = Anio {
+    anio = 2022,
+    eventos = [crisis, remodelar 5, reevaluar 7]
 }
 
--- Issue 1
--- Elimino subtract y lo reemplazo por una suma, y ademas anioFundacion ciudad lo convierto en negativo
--- Es correcto asi?
-valorCiudad :: Ciudad -> Number
-valorCiudad ciudad | anioFundacion ciudad < 1800          =  (+1800) (- anioFundacion ciudad)
-                   | atraccionesPrincipales ciudad == []  = costoDeVida ciudad *2
-                   | otherwise                            = costoDeVida ciudad *3
+dosmilveintiuno = Anio {
+    anio = 2021,
+    eventos = [crisis, agregarNuevaAtraccion "Playa"]
+}
+
+dosmilquince = Anio {
+    anio = 2015,
+    eventos = []
+}
+
+aplicarEventos ciudad eventos = foldl (\ciudad evento -> evento ciudad) ciudad eventos
+
+pasoDelAnio anio ciudad = aplicarEventos ciudad (eventos anio)
+-- para foldl la funcion recibe primero la seed y luego el elemento de la lista, para acumularlo (left)
+
+-- otra opcion puede ser con foldr -> foldr ($) ciudad (eventos anio)
+-- para foldr la funcion primero recibe el elemento de la lista y luego la seed, para acumularlo (right)
 
 
--- Issue 2.1
--- Cambio de nombres a unos mas expresivos. Son adecuados?
-esVocal :: Char -> Bool
-esVocal letra = letra `elem` "aeiouAEIOU"
+{- Punto 5.2
+    Implementar una función que reciba una ciudad, un criterio de comparación y un evento, de manera que nos diga si la ciudad tras el evento subió respecto a ese criterio
+ -}
 
-esUnaAtraccionCopada :: String -> Bool
-esUnaAtraccionCopada (primeraLetra:_) = esVocal primeraLetra
+segunCostoDeVida = costoDeVida 
 
-tieneAtraccionesCopadas :: LaCiudadEs
-tieneAtraccionesCopadas ciudad = any esUnaAtraccionCopada (atraccionesPrincipales ciudad)
+segunCantidadDeAtracciones = length . atraccionesPrincipales 
+
+algoMejor ciudad criterio evento = criterio (evento ciudad) > criterio ciudad
 
 
--- Issue 2.2
-aumentarPorcentaje :: Number -> Number -> Number
-aumentarPorcentaje aumento costo = aumento * costo / 100 + costo
+{- Punto 5.3
+    Para un año, queremos aplicar sobre una ciudad solo los eventos que hagan que el costo de vida suba. Debe quedar como resultado la ciudad afectada con dichos eventos.
+ -}
 
--- bajarCostodeVida ahora recibe una Ciudad y retorna un numero. Es correcto?
--- O es preferible no modificar la funcion y solo cambiar el nombre a "reducir porcentaje"?
-bajarCostodeVida :: Ciudad -> Number 
-bajarCostodeVida ciudad = aumentarPorcentaje (-10) (costoDeVida ciudad)
+soloSubeCostoDeVida anio ciudad = aplicarEventos ciudad . filter (algoMejor ciudad segunCostoDeVida) $ eventos anio
 
--- Cambio de nombres en la recursividad. Es más expresivo de esta forma?
-quitarUltimaAtraccion :: [a] -> [a]
-quitarUltimaAtraccion [] = []  -- caso base
-quitarUltimaAtraccion [primeraAtraccion] = [] -- caso unitario
-quitarUltimaAtraccion (primeraAtraccion : atracciones) = primeraAtraccion : quitarUltimaAtraccion atracciones  -- recursividad
 
-invocarCrisis :: Ciudad -> Ciudad
-invocarCrisis ciudad = ciudad {
-                        atraccionesPrincipales = quitarUltimaAtraccion (atraccionesPrincipales ciudad),
-                        costoDeVida            = bajarCostodeVida ciudad
-                    }
+{- Punto 5.4
+    Para un año, queremos aplicar solo los eventos que hagan que el costo de vida baje. Debe quedar como resultado la ciudad afectada con dichos eventos.
+ -}
+
+algoPeor ciudad criterio evento = criterio (evento ciudad) < criterio ciudad
+
+soloBajaCostoDeVida anio ciudad = aplicarEventos ciudad . filter (algoPeor ciudad segunCostoDeVida) $ eventos anio
+
+
+{- Punto 5.5
+    Para un año, queremos aplicar solo los eventos que hagan que el valor suba. Debe quedar como resultado la ciudad afectada con dichos eventos.
+ -}
+
+-- Utiliza la funcion soloSubeCostoDeVida del punto 5.3
+
+
+{- Punto 6.1
+    Dado un año y una ciudad, queremos saber si los eventos están ordenados en forma correcta, esto implica que el costo de vida al aplicar cada evento se va incrementando respecto al anterior evento. Debe haber al menos un evento para dicho año.
+ -}
+
+segunEventos ciudad evento = costoDeVida (evento ciudad)
+
+segunCiudades evento ciudad = costoDeVida (evento ciudad)
+
+segunAnios ciudad anio = costoDeVida (pasoDelAnio anio ciudad)
+
+verificarOrden _ _ []  = False
+verificarOrden _ _ [x] = True
+verificarOrden segun algo (x:y:ys) = segun algo x < segun algo y && verificarOrden segun algo (y:ys)
+
+eventosOrdenados anio ciudad = verificarOrden segunEventos ciudad (eventos anio)
+
+{- Punto 6.2
+    Dado un evento y una lista de ciudades, queremos saber si esa lista está ordenada. Esto implica que el costo de vida al aplicar el evento sobre cada una de las ciudades queda en orden creciente. Debe haber al menos una ciudad en la lista.
+ -}
+
+ciudadesOrdenadas evento ciudades = verificarOrden segunCiudades evento ciudades 
+
+
+{- Punto 6.3
+    Dada una lista de años y una ciudad, queremos saber si el costo de vida al aplicar todos los eventos de cada año sobre esa ciudad termina generando una serie de costos de vida ascendentes (de menor a mayor). Debe haber al menos un año en la lista.
+ -}
+
+aniosOrdenados ciudad anios = verificarOrden segunAnios ciudad anios
--- proyecto-test/src/Spec.hs
+++ proyecto-test/src/Spec.hs
@@ -3,9 +3,149 @@ import PdePreludat
 import Library
 import Test.Hspec
 
-{- correrTests :: IO ()
+correrTests :: IO ()
 correrTests = hspec $ do
-  describe "Test de ejemplo" $ do
-    it "El pdepreludat se instaló correctamente" $ do
-      doble 1 `shouldBe` 2 -}
+--Punto 1
+  describe "Utilización de valorCiudad/1" $ do
+    it "Si el año de fundacion es antes de 1800, entonces, se incrementa 5 veces el valor" $ do
+      valorCiudad baradero `shouldBe` 925
+    it "Si, no tiene atracciones, el valor se duplica" $ do
+      valorCiudad nullish `shouldBe` 280 
+    it "Sino el valor se triplica " $ do
+      valorCiudad caletaOlivia `shouldBe` 360 
 
+
+--Punto 2.1
+--Tiene Atracciones Copadas?
+  describe "Utilización de tieneAtraccionesCopadas" $ do
+    it "La ciudad Baradero no tiene atracciones que comiencen con una vocal, por lo que no es copada" $ do
+      tieneAtraccionesCopadas baradero  `shouldBe` False
+    it "La ciudad Nullish no tiene atracciones siquiera, por lo que no es copada" $ do
+      tieneAtraccionesCopadas nullish  `shouldBe` False
+    it "La ciudad Caleta Olivia tiene una atraccion que comienza con una vocal, por lo que es copada" $ do
+      tieneAtraccionesCopadas caletaOlivia  `shouldBe` True
+
+
+--Punto 2.2
+--Ciudad sobria
+  describe "Utilización de ciudadSobria/2" $ do
+    it "La ciudad Baradero tiene atracciones con 15 y 24 letras que son mas de 14 por lo tanto es ciudad sobria" $ do
+      ciudadSobria 14 baradero  `shouldBe` True 
+    it "La ciudad Baradero tiene atracciones con 15 y 24 letras una exactamente igual a 15 por lo tanto no es ciudad sobria" $ do
+      ciudadSobria 15 baradero  `shouldBe` False 
+    it "La ciudad Nullish no tiene atracciones por lo tanto no es ciudad sobria" $ do
+      ciudadSobria 5 nullish  `shouldBe` True
+
+
+--Punto 2.3
+-- Ciudad de Nombre Raro
+  describe "Utilización de ciudadDenombreRaro/1" $ do
+    it "La ciudad Azul tiene 4 letras por lo tanto tiene nombre raro" $ do
+      tieneNombreRaro azul `shouldBe` True 
+    it "La ciudad Maipu tiene 5 letras por lo tanto no tiene nombre raro" $ do
+      tieneNombreRaro maipu `shouldBe` False 
+
+
+--Punto 3      
+  describe "Utilización de agregarNuevaAtraccion/2" $ do
+    it "La ciudad debe tener 4 atracciones y su costo de vida debe quedar en 228" $ do
+       atraccionesPrincipales (agregarNuevaAtraccion "Balneario Municipal Alte. Guillermo Brown" azul) `shouldBe` ["Balneario Municipal Alte. Guillermo Brown" ,"Teatro Español", "Parque Municipal Sarmiento", "Costanera Cacique Catriel"]
+
+--Punto 3.1
+--Invocar Crisis
+  describe "Utilización de invocarCrisis" $ do
+    it "Si se invoca una crisis sobre azul debe bajar un 10% su costo de vida" $ do
+        costoDeVida(crisis azul) `shouldBe` 171
+    it "Si se invoca una crisis sobre azul debe cerrar la ultima atraccion" $ do
+        atraccionesPrincipales(crisis azul)
+        `shouldBe` ["Teatro Español", "Parque Municipal Sarmiento"]
+
+--Punto 3.2                                                                                       
+--Remodelar ciudad 
+  describe "Utilización de remodelarCiudad/2" $ do
+    it "La ciudad debe tener quedar con un nombrede New Azul y su costo de vide debe aumentar a 285" $ do
+       nombre (remodelar 50 azul) `shouldBe`  "New Azul" 
+       costoDeVida(remodelar 50 azul) `shouldBe`   285 
+
+--Punto 3.3        
+-- Reevaluacion
+  describe "Utilización dereevaluandoElValor/2" $ do
+    it "Si la ciudad Azul no es sombria, baja a un coste de vida de 187" $ do
+      costoDeVida (reevaluar 14 azul) `shouldBe` 187
+    it "Si la ciudad Azul es sombria, aumenta su costo de vida a 209" $ do
+      costoDeVida (reevaluar 13 azul) `shouldBe` 209 
+
+
+--Punto 5.1 
+--Los años pasan
+  describe "Utilizacion de aplicarPasoDelAnio/2" $ do
+    it "Debe quedar con el nombre  New Azul, un costo de vida de 197.505 y perder la ultima atraccion" $ do
+      nombre (pasoDelAnio dosmilveintidos azul) `shouldBe` "New Azul"
+      costoDeVida (pasoDelAnio dosmilveintidos azul) `shouldBe` 197.505
+      atraccionesPrincipales (pasoDelAnio dosmilveintidos azul) `shouldBe` ["Teatro Español", "Parque Municipal Sarmiento"]
+
+  describe "Utilizacion de aplicarPasoDelAnio/2" $ do
+    it "Debe quedar con el mismo  costo de vida " $ do
+      costoDeVida (pasoDelAnio dosmilquince azul) `shouldBe` costoDeVida azul
+
+
+--Punto 5.2 
+--Algo Mejor
+  describe "Utilizacion de esAlgoMejor/3" $ do
+    it "Para la ciudad azul, el criterio costo de vida y el evento crisis, la ciudad no subio respecto a ese criterio " $ do
+      algoMejor azul segunCostoDeVida crisis `shouldBe` False
+    it "Para la ciudad azul, el criterio costo de vida y el evento agregar atraccion 'Monasterio Trapense', la ciudad subio respecto a ese criterio " $ do
+      algoMejor azul segunCostoDeVida (agregarNuevaAtraccion "Monasterio Trapense") `shouldBe` True
+    it "Para la ciudad azul, el criterio cantidad de atracciones y el evento agregar atraccion 'Monasterio Trapense', la ciudad subio respecto a ese criterio " $ do
+      algoMejor azul segunCantidadDeAtracciones (agregarNuevaAtraccion "Monasterio Trapense") `shouldBe` True
+
+
+--Punto 5.3 
+--Costo de vida que suba
+  describe "Utilizacion de soloSubirCostoDeVida/2" $ do
+    it "Para el año 2022 y la ciudad azul, se deben aplicar solo los eventos que hagan que el costo de vida suba" $ do
+      nombre (soloSubeCostoDeVida dosmilveintidos azul) `shouldBe` "New Azul"
+      costoDeVida (soloSubeCostoDeVida dosmilveintidos azul) `shouldBe` 219.45
+
+
+--Punto 5.4 
+--Costo de vida que baje 
+  describe "Utilizacion de soloBajarCostoDeVida/2" $ do
+    it "Debe quedar con el mismo nombre Azul, un costo de vida de 171 y perder la ultima atraccion" $ do
+      nombre (soloBajaCostoDeVida dosmilveintidos azul) `shouldBe` "Azul"
+      costoDeVida (soloBajaCostoDeVida dosmilveintidos azul) `shouldBe` 171
+      atraccionesPrincipales (soloBajaCostoDeVida dosmilveintidos azul) `shouldBe` ["Teatro Español", "Parque Municipal Sarmiento"]
+
+
+--Punto 5.5 
+--sube el valor
+  describe "Utilización de soloSubirValor/2" $ do
+    it "La ciudad debe tener quedar con el nombrede New Nullish  y su costo de vide debe quedar en 161.7" $ do
+      nombre (soloSubeCostoDeVida dosmilveintidos nullish) `shouldBe`  "New Nullish" 
+      costoDeVida (soloSubeCostoDeVida dosmilveintidos nullish) `shouldBe`  161.7
+
+
+--Punto 6.1
+--Eventos Ordenados
+  describe "Utilizacion de losEventosEstanOrdenados/2" $ do
+    it "Para el año 2022 y la ciudad azul, los eventos estan ordenados de forma correcta" $ do
+      eventosOrdenados dosmilveintidos azul `shouldBe` True
+    it "Para el año 2023 y la ciudad azul, los eventos no estan ordenados de forma correcta" $ do
+      eventosOrdenados dosmilveintitres azul `shouldBe` False
+
+--Punto 6.2
+--Ciudades Ordenadas
+  describe "Utilización de lasCiudadesEstanOrdenadas/2" $ do
+    it "Al aplicar el evento remodelarCiudad 10 la lista de ciudades debe quedar en orden creciente segun costo de vida" $ do
+      ciudadesOrdenadas (remodelar 10) [caletaOlivia,nullish,baradero,azul] `shouldBe` True
+  describe "Utilización de estaOrdenadaSegunCostoDeVida/2" $ do
+    it "Al aplicar el evento remodelarCiudad 10 la lista de ciudades no queda en orden creciente segun costo de vida" $ do
+      ciudadesOrdenadas (remodelar 10) [caletaOlivia,azul,baradero] `shouldBe` False 
+         
+--Punto 6.3
+--Anios Ordenados
+  describe "Utilización de losAniosEstanOrdenados/2" $ do
+    it "Que los años 2021, 2022 y 2023 estén ordenados para Baradero da false" $ do
+      aniosOrdenados baradero [dosmilveintiuno,dosmilveintidos,dosmilveintitres] `shouldBe` False 
+    it "Que los años 2022, 2021 y 2023 estén ordenados para Baradero, de True, porque, al aplicar los años en ese orden va subiendo el costo de vida" $ do
+      aniosOrdenados baradero [dosmilveintidos,dosmilveintiuno,dosmilveintitres] `shouldBe` True 
